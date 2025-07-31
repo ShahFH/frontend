ce86df7660e77a20797bb32d01201702cd432df0
ShahFH <shahfaisal.swe@gmail.com>
2025-07-31 13:25:07 +0430

    added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'text',
+  description: 'test',
 }
 
 export default function RootLayout({
@@ -17,13 +16,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
-        `}</style>
       </head>
       <body>{children}</body>
     </html>
